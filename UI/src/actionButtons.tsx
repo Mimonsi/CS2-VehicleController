2e7fa8dd09a71a7d818f7edd879cd7f2bb12de48
Konsi / Mimonsi <konstantin-schwarze@outlook.de>
2025-07-27 20:59:32 +0200

    Group action buttons in panel

--- UI/src/actionButtons.tsx
+++ UI/src/actionButtons.tsx
@@ -2,7 +2,8 @@ import React from "react";
 import { trigger } from "cs2/api";
 import { ModuleResolver } from "moduleResolver";
 import mod from "../mod.json";
-import {getModule} from "cs2/modding";
+import { getModule } from "cs2/modding";
+import styles from "./actionButtons.module.scss";
 
 const InfoButton = getModule(
   "game-ui/game/components/selected-info-panel/shared-components/info-button/info-button.tsx",
@@ -14,29 +15,46 @@ const InfoButton = getModule(
 // city-wide copying (global)
 export const ActionButtons = () => {
 
-  const buttons = [
-    { label: "Copy Selection", event: "CopySelectionClicked" },
-    { label: "Paste Same Prefab", event: "PasteSamePrefabClicked" },
-    { label: "Paste Service Type", event: "PasteServiceTypeClicked" },
-    { label: "Paste District", event: "PasteDistrictClicked" },
-    { label: "Paste City", event: "PasteCityClicked" },
-    { label: "Export", event: "ExportClipboardClicked" },
-    { label: "Import", event: "ImportClipboardClicked" },
+  const groups = [
+    {
+      title: "Copy & Paste",
+      buttons: [
+        { label: "Copy Selection", event: "CopySelectionClicked" },
+        { label: "Paste Same Prefab", event: "PasteSamePrefabClicked" },
+        { label: "Paste Service Type", event: "PasteServiceTypeClicked" },
+        { label: "Paste District", event: "PasteDistrictClicked" },
+        { label: "Paste City", event: "PasteCityClicked" },
+      ],
+    },
+    {
+      title: "Import / Export",
+      buttons: [
+        { label: "Export", event: "ExportClipboardClicked" },
+        { label: "Import", event: "ImportClipboardClicked" },
+      ],
+    },
   ];
   
   return (
     <>
       {InfoButton ? (
-        buttons.map((b) => (
+        groups.map((group) => (
           <ModuleResolver.instance.InfoRow
-            left={
-              <InfoButton
-                key={b.event}
-                label={b.label}
-                icon={"Media/Game/Icons/GenericVehicle.svg"}
-                selected={false}
-                onSelect={() => trigger(mod.id, b.event)}
-              />
+            key={group.title}
+            left={group.title}
+            uppercase={true}
+            right={
+              <div className={styles.buttonGroup}>
+                {group.buttons.map((b) => (
+                  <InfoButton
+                    key={b.event}
+                    label={b.label}
+                    icon={"Media/Game/Icons/GenericVehicle.svg"}
+                    selected={false}
+                    onSelect={() => trigger(mod.id, b.event)}
+                  />
+                ))}
+              </div>
             }
             disableFocus={true}
           />
