cc1c0f0d5998c4980f0f94d341bd2f8dc09487cd
Mimonsi <konstantin-schwarze@outlook.de>
2026-07-05 13:29:49 +0200

    Add Logo

--- UI/src/manager/VehicleManager.tsx
+++ UI/src/manager/VehicleManager.tsx
@@ -2,8 +2,9 @@ import { FloatingButton, Portal } from "cs2/ui";
 import React, { useState } from "react";
 
 import { VehicleManagerPanel } from "./VehicleManagerPanel";
+import vcLogo from "./VC.png";
 
-const buttonSrc = "coui://uil/Standard/Car.svg";
+const buttonSrc = vcLogo;
 
 // Appended to "GameTopRight": a toggle button that opens the floating
 // Vehicle Manager window. Open state lives here (M2 prototype has no C# yet);
@@ -15,6 +16,7 @@ export const VehicleManager = () => {
     <>
       <FloatingButton
         src={buttonSrc}
+        tinted
         selected={open}
         tooltipLabel={"Vehicle manager"}
         onSelect={() => setOpen(o => !o)}
